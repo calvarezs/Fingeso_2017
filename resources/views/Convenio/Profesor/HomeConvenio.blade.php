4ffb34a2d3551a09c18edc18282e6c8bcb431bde
Jhorvis Sanchez <jhorvissanchez2@gmail.com>
2017-12-03 16:27:09 -0300

    h

--- resources/views/Convenio/Profesor/HomeConvenio.blade.php
+++ resources/views/Convenio/Profesor/HomeConvenio.blade.php
@@ -21,17 +21,15 @@
  <br>
   <div class="jumbotron" align="center"  >
     <h1>CONVENIO ACTUAL</h1> 
-    <p>  <a href="{{ route('VerConvenio') }}" class="btn btn-primary"> VER </a> </p> 
-     <p> o que aparezca crear si no existe </p>
-
-     <p>  <a href="{{ route('CreateConvenio') }}"  class="btn btn-primary"> CREAR</a> </p> 
+    <p> <a href="{{ route('VerConvenio') }}" class="btn btn-primary">VER   ACTUAL</a> </p> 
+    <p> <a href="{{ route('CreateConvenio') }}"  class="btn btn-primary">CREAR NUEVO</a> </p> 
   </div>
                
  <br>
   <div class="jumbotron " align="center">
     <h1>HISTORIAL DE CONVENIOS</h1> 
 
-    <p>  <a href="{{ route('historialConvenio') }}"  class="btn btn-primary"> VER</a> </p> 
+    <p>  <a href="{{ route('historialConvenio') }}"  class="btn btn-primary"> VER HISTORIAL</a> </p> 
   </div>
 </div>      
 
